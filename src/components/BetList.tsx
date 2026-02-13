f54b11bef4e0ef5860694d58d6a41b8cb1e6ecfe
Jack Bleiweis <jackbleiweis@hotmail.com>
2026-02-13 18:57:20 -0500

    Restore betlist

---
+++ src/components/BetList.tsx
@@ -0,0 +1,19 @@
+import type { Bet } from '../types/bet'
+import BetCard from './BetCard'
+
+type Props = { bets: Bet[] }
+
+export default function BetList({ bets }: Props) {
+  return (
+    <div className="bet-list">
+      <div className="bet-list__grid">
+        {bets.map(bet => (
+          <BetCard key={bet.id} bet={bet} />
+        ))}
+      </div>
+      {bets.length === 0 && (
+        <p className="bet-list__empty">No bets match the filters.</p>
+      )}
+    </div>
+  )
+}
